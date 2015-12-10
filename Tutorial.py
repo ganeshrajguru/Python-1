f9e14c9ae509541dd733d55dc8c73082e20ac14b
sanjitmisra <misra.sanjit@gmail.com>
2015-12-10 23:29:03 +0530

    First Commit

---
+++ Tutorial.py
@@ -0,0 +1,16 @@
+import random
+import sys
+import os
+
+print("hello world")
+
+to_do_list = ['Bananas', 'Apples']
+print(to_do_list[0])
+my_string = 'Bangalore'
+length = len(my_string)
+
+i = length - 1
+for x in range(0, length-1):
+    i -= 1
+    print(my_string[i])
+
