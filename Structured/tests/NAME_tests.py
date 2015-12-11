bafad9c54c66a2857098b19f85b8a8b4c3d74843
sanjitmisra <misra.sanjit@gmail.com>
2015-12-12 00:55:10 +0530

    Added another TC - 3rd Test Case

--- Structured/tests/NAME_tests.py
+++ Structured/tests/NAME_tests.py
@@ -24,3 +24,18 @@ def test_room_paths():
     center.add_paths({'north': north, 'south': south})
     assert_equal(center.go('north'), north)
     assert_equal(center.go('south'), south)
+
+
+def test_map():
+    start = Room("Start", "You start your journey from here.")
+    west = Room("West", "You have moved westwards. Turn East for greener pastures!!")
+    bottom = Room("Bottom", "You just hit rock bottom. Nowhere to go but up!!")
+
+    start.add_paths({'west': west, 'down': bottom})
+    west.add_paths({'east': start})
+    bottom.add_paths({'up': start})
+
+    assert_equal(start.go('west'), west)
+    assert_equal(start.go('west').go('east'), start)
+    assert_equal(start.go('down').go('up'), start)
+
