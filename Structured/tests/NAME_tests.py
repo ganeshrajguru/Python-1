ebc315a676aa8b3a3ae5a6add6eceebed182235a
ganeshrajguru <ganeshrajguru1@gmail.com>
2015-12-13 17:27:12 +0530

    Committing to my forked repo - Try 2

--- Structured/tests/NAME_tests.py
+++ Structured/tests/NAME_tests.py
@@ -52,4 +52,4 @@ def test_map_1():
     assert_equal(start.go('west'), west)
     assert_equal(start.go('west').go('east'), start)
     assert_equal(start.go('down').go('up'), start)
-
+    assert_equal(start.go('down').go('up'), start)
